8a9dd3779f92196b6168d2527fa0e1fbc6e2e5e2
zhouziyi <zhouziyi@corp.netease.com>
2018-07-21 11:28:43 +0800

    add content

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-docker run -v "$PWD:/usr/src/app" -it wiki-deploy
-docker ps -a | awk 'NR==2{print $1}' | xargs docker rm
+docker run --rm -v "$PWD:/usr/src/app" -ti wiki-deploy
 git add -A && git commit -m "add content" && git push
--- docs/deploy.sh
+++
@@ -1,3 +0,0 @@
-docker run -v "$PWD:/usr/src/app" -it wiki-deploy
-docker ps -a | awk 'NR==2{print $1}' | xargs docker rm
-git add -A && git commit -m "add content" && git push
